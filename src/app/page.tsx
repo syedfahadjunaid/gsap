c25d9643794824583d7197d8b679bec252d83e0a
syedfahadjunaid <syedfahadjunaid@gmail.com>
2025-02-14 19:15:17 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+import gsap from "gsap";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 export default function Home() {
-  return <div>First Page</div>;
+  const container = useRef<HTMLDivElement>(null);
+  const circle = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      gsap.to(".box", { rotation: "+360", duration: 3 });
+      gsap.to(circle.current, { rotation: "-=360", duration: 3 });
+    },
+    { scope: container }
+  );
+  return (
+    <div className="App">
+      <div ref={container} className="container">
+        <div className="box gradient-blue">selector</div>
+        <div className="box gradient-blue">selector</div>
+        <div className="circle gradient-green" ref={circle}>
+          Ref
+        </div>
+      </div>
+      <div className="box gradient-blue">selector</div>
+    </div>
+  );
 }
